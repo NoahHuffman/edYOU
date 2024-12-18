816defa0e7d8824c904680418128c6e6f90443f4
Noah Huffman <noahhuffman05@gmail.com>
2024-12-18 14:17:34 -0500

    use my user id and course id

--- api/canvasApis.ts
+++ api/canvasApis.ts
@@ -1,12 +1,12 @@
-import { CANVAS_KEY } from "@/api/constants";
+import { CANVAS_KEY, USER_ID, CPSC_4910_ID } from "@/api/constants";
 import { Assignment } from "@/api/interfaces";
 
 const accessToken = CANVAS_KEY;
 // const canvasUrl = 'https://utchattanooga.instructure.com/api/v1/planner/items?order=asc&start_date=2024-12-04T05%3A00%3A00.000Z&page=bookmark:WyJ2aWV3aW5nIixbIjIwMjQtMTItMDkgMDQ6NTk6NTkuMDAwMDAwIiw4NjY3NjFdXQ&per_page=10';
 const canvasUrl =
-  "https://utchattanooga.instructure.com/api/v1/users/self/courses";
+  `https://utchattanooga.instructure.com/api/v1/users/${USER_ID}/courses`;
 const assignmentsUrl =
-  "https://utchattanooga.instructure.com/api/v1/users/self/courses/36686/assignments";
+  `https://utchattanooga.instructure.com/api/v1/users/${USER_ID}/courses/${CPSC_4910_ID}/assignments`;
 
 export const fetchCourses = async () => {
   try {
