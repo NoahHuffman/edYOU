d77b4d03404210124a4209f7f11077393ead51ea
Noah Huffman <noahhuffman05@gmail.com>
2024-12-18 09:23:24 -0500

    calendar

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Button } from "react-native";
-import ZipCodePopup from "../components/ZipComponent";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { View, StyleSheet } from "react-native";
+import { CalendarList } from "react-native-calendars";
 
 const HomeScreen = () => {
-  const [isPopupVisible, setPopupVisible] = useState(false);
+  const [markedDates, setMarkedDates] = useState({});
+  const todaysDate = new Date().toISOString().split('T')[0];
+
+  const onDayPress = (day: { dateString: any; }) => {
+    const markedDates = {
+      [day.dateString]: {
+        selected: true,
+        marked: true,
+        selectedColor: "blue",
+      },
+    };
+
+    setMarkedDates(markedDates);
+  };
 
   return (
-    <Calendar
-      style={styles.calendar}
-      // Specify the current date
-      current={"2012-03-01"}
-      // Callback that gets called when the user selects a day
-      onDayPress={(day: any) => {
-        console.log("selected day", day);
-      }}
-      // Mark specific dates as marked
-      markedDates={{
-        "2012-03-01": { selected: true, marked: true, selectedColor: "blue" },
-        "2012-03-02": { marked: true },
-        "2012-03-03": { selected: true, marked: true, selectedColor: "blue" },
-      }}
-    />
+    <View style={styles.container}>
+      <CalendarList
+        style={styles.calendar}
+        current={todaysDate}
+        onDayPress={onDayPress}
+        markedDates={markedDates}
+        horizontal={false}
+        pagingEnabled={false}
+        pastScrollRange={12}
+        futureScrollRange={12}
+      />
+    </View>
   );
 };
 
@@ -33,18 +41,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  button: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: "#007BFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   calendar: {
     borderWidth: 1,
     borderColor: "gray",
-    height: 100,
+    height: "100%",
+    width: "100%",
   },
 });
 
