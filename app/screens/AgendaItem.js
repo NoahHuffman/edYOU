95d685f74673aeecc2df915620854e244759c897
Noah Huffman <noahhuffman05@gmail.com>
2025-01-27 15:17:11 -0500

    add go to assignments button

--- app/screens/AgendaItem.js
+++ app/screens/AgendaItem.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Dimensions } from "react-native";
-import RenderHTML from 'react-native-render-html';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  Linking,
+  StyleSheet,
+} from "react-native";
+import RenderHTML from "react-native-render-html";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export const AgendaItem = ({
   assignmentName,
@@ -10,7 +18,7 @@ export const AgendaItem = ({
   backgroundColor,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const { width } = Dimensions.get('window');
+  const { width } = Dimensions.get("window");
 
   return (
     <View
@@ -34,10 +42,42 @@ export const AgendaItem = ({
           <View
             style={{ height: 1, backgroundColor: "gray", marginVertical: 5 }}
           />
-          {/* <Text>{description}</Text> */}
-          <RenderHTML contentWidth={width} source={{ html: description }} ignoredDomTags={['link']} />
+          <View style={styles.container}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => Linking.openURL("http://google.com")} // @TODO: assignment.html_url
+            >
+              <Text style={styles.buttonText}>Go to assignment</Text>
+              <Icon name="external-link" size={16} color="white" />
+            </TouchableOpacity>
+          </View>
+          <RenderHTML
+            contentWidth={width}
+            source={{ html: description }}
+            ignoredDomTags={["link"]}
+          />
         </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "blue",
+    borderRadius: 5,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    marginRight: 5,
+  },
+});
