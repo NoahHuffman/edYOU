4a3e7d85b51b8c50e216f1284ebe5a956c6fa75e
Noah Huffman <noahhuffman05@gmail.com>
2025-03-09 17:24:29 -0400

    add StatusBar

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import { useState } from "react";
+import { StatusBar } from "react-native";
 
 const homeName = "Home";
 const settingsName = "Settings";
@@ -17,52 +18,55 @@ function MainContainer() {
   const [colorChanged, setColorChanged] = useState(false);
 
   return (
-    <Tab.Navigator
-      initialRouteName={homeName}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          let rn = route.name;
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-          if (rn === homeName) {
-            iconName = focused ? "home" : "home-outline";
-          } else {
-            iconName = focused ? "settings" : "settings-outline";
-          }
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else {
+              iconName = focused ? "settings" : "settings-outline";
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "grey",
-        tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
-        tabBarStyle: { padding: 10, height: 70 },
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen
-        name={homeName}
-        children={() => (
-          <HomeScreen
-            currentCourses={currentCourses}
-            setCurrentCourses={setCurrentCourses}
-            colorChanged={colorChanged}
-            setColorChanged={setColorChanged}
-          />
-        )}
-        options={{ tabBarLabel: "" }}
-      />
-      <Tab.Screen
-        name={settingsName}
-        children={() => (
-          <SettingsScreen
-            currentCourses={currentCourses}
-            setCurrentCourses={setCurrentCourses}
-            setColorChanged={setColorChanged}
-          />
-        )}
-        options={{ tabBarLabel: "" }}
-      />
-    </Tab.Navigator>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "grey",
+          tabBarLabelStyle: { paddingBottom: 10, fontSize: 10 },
+          tabBarStyle: { padding: 10, height: 70 },
+          headerShown: false,
+        })}
+      >
+        <Tab.Screen
+          name={homeName}
+          children={() => (
+            <HomeScreen
+              currentCourses={currentCourses}
+              setCurrentCourses={setCurrentCourses}
+              colorChanged={colorChanged}
+              setColorChanged={setColorChanged}
+            />
+          )}
+          options={{ tabBarLabel: "" }}
+        />
+        <Tab.Screen
+          name={settingsName}
+          children={() => (
+            <SettingsScreen
+              currentCourses={currentCourses}
+              setCurrentCourses={setCurrentCourses}
+              setColorChanged={setColorChanged}
+            />
+          )}
+          options={{ tabBarLabel: "" }}
+        />
+      </Tab.Navigator>
+    </>
   );
 }
 
